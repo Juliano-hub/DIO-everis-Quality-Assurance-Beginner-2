18c5483eeb80348c672feecd9867ba13de046b87
Juliano-hub <juliano.buss@gmail.com>
2021-08-05 11:01:52 -0300

    Delete src directory

--- src/Básico/Herança/main.java
+++
@@ -1,9 +0,0 @@
-package Básico.Herança;
-
-public class main {
-    public static void main(String[] args){
-        Carro carroUm = new Carro("Azul", "Fiat", 30000, 4);
-        System.out.println(carroUm.toString());
-
-    }
-}
